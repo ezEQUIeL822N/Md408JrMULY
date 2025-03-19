a984b156a85e5f2b5bf15d50534d863304bcc8e9
ezEQUIeL822N <freight.scurry-0e@icloud.com>
2025-03-19 16:12:32 +0000

    Improve the file's modularity by adding new classes

--- r0Ngv/xFl4z/0PUXDP/confluence_fTlq1_1741447469.txt.js
+++ r0Ngv/xFl4z/0PUXDP/confluence_fTlq1_1741447469.txt.js
@@ -4,7 +4,6 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     'no-var': 'error', // 不能使用var声明变量
-    'import/extensions': 'off',
     'linebreak-style': [0, 'error', 'windows'],
     'space-before-function-paren': 0, // 在函数左括号的前面是否有空格
     'no-console': ['error', { allow: ['log', 'warn'] }], // 允许使用console.log()
